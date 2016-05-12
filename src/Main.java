cb9fbdcc7f2ebddb6f0e95c88ff2c431097c3db1
Adi <adelina_yordanova11@abv.bg>
2016-05-12 21:50:32 +0300

    Exapmle03_1.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Въведете число: ");
+		int a = in.nextInt();
+
+		if (a < 0 && a % 2 == 0) {
+			System.out.println("Отрицателно четно.");
+
+		} else if (a < 0 && a % 2 == 1) {
+			System.out.println("Отрицателно нечетно.");
+
+		} else if (a > 0 && a % 2 == 0) {
+			System.out.println("Положително четно.");
+
+		} else if (a > 0 && a % 2 == 1) {
+			System.out.println("Положително нечетно.");
+
+		}else{
+			System.out.println("Нула.");
+		}
 
 	}
 
